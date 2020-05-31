4ad6c9d0de25d40b5cc912ddcdde55f73f1b3040
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-05-31 11:19:06 +0300

    Update manage.py
    Added function "update_city"

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 # Autor: Stepan Skriabin - stepan.skrjabin@gmail.com
 # Manger
 import sqlobject as orm
-import botconfig
+import time
 import click
 
+import botconfig
 import models
+from botbase import update_city_list
 
-connection = orm.connectionForURI(botconfig.ONLINE_POSTGRESQL)
+connection = orm.connectionForURI(botconfig.LOCAL_SQLITE)
 orm.sqlhub.processConnection = connection
 
 
@@ -15,13 +17,16 @@ orm.sqlhub.processConnection = connection
     whit all data. The operation, by default, creates all tables')
 def main(table):
     if table == "create":
+        start_time = time.process_time()
         models.Users.createTable(ifNotExists=True)
         models.UsersSettings.createTable(ifNotExists=True)
         models.CurrentWeather.createTable(ifNotExists=True)
         models.ForecastWeather.createTable(ifNotExists=True)
         models.OnecallWeather.createTable(ifNotExists=True)
         models.CityList.createTable(ifNotExists=True)
+        print(f"Table is createt at: {time.process_time() - start_time} sec.")
     elif table == "delete":
+        start_time = time.process_time()
         models.Users.dropTable(
             ifExists=True, dropJoinTables=True, cascade=True)
         models.UsersSettings.dropTable(
@@ -34,17 +39,12 @@ def main(table):
             ifExists=True, dropJoinTables=True, cascade=True)
         models.CityList.dropTable(
             ifExists=True, dropJoinTables=True, cascade=True)
-    else:
-        return
-
-
-@click.command()
-@click.option('--city', help='download city list')
-def fill_table(city):
-    if city == 'update':
-        pass
-    elif city == 'delete':
-        pass
+        print(f"Table is deleted at: {time.process_time() - start_time} sec.")
+    elif table == 'update_city':
+        start_time = time.process_time()
+        update_city_list(botconfig.CITY_LIST)
+        print(f'List of cities is loaded at: \
+            {time.process_time() - start_time} sec.')
     else:
         return
 
