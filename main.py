06746ac3433c891927041bf89c65759445353204
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-05-31 17:22:37 +0300

    Added the function of building a menu.
    Menu items are not yet working. The old code has been removed.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ __version__ = '0.0.9b1'
 
 from telebot import TeleBot
 from telebot.types import Message
-from telebot.types import InlineQueryResultArticle
-from telebot.types import InputTextMessageContent
+from telebot.types import ReplyKeyboardMarkup
+from telebot.types import ReplyKeyboardRemove
+from telebot.types import KeyboardButton
 from translitua import translit
 from translitua import RussianInternationalPassport1997
 import sqlobject as orm
@@ -18,6 +19,7 @@ import botconfig
 from botsearch import search_weather
 from botsearch import replace_name
 from models import Users
+from models import UsersSettings
 from models import CurrentWeather
 from models import ForecastWeather
 from models import OnecallWeather
@@ -31,15 +33,15 @@ bot = TeleBot(botconfig.TELEGRAM_TOKEN_API)
 
 """
     Choose the option to connect to the database, where:
-    LOCAL_SQLITE: the bot_database.db file will be created
+    LOCAL_SQLITE: bot_database.db file will be created
     in the local directory
-    LOCAL_POSTGRESQL: a connection to a local server will be made
-    ONLINE_POSTGRESQL: a connection will be made to a server hosted
+    LOCAL_POSTGRESQL: connection to a local server
+    ONLINE_POSTGRESQL: connection will be made to a server hosted
     on the Internet.
 
     The settings are in the file botconfig.py
 """
-connection = orm.connectionForURI(botconfig.ONLINE_POSTGRESQL)
+connection = orm.connectionForURI(botconfig.LOCAL_SQLITE)
 orm.sqlhub.processConnection = connection
 
 
@@ -82,7 +84,7 @@ def handler_command_adduser(message: Message):
             message.from_user.language_code,
             message.from_user.is_bot]
     dbquery = Users.select(
-        Users.q.userID == data[0])
+        Users.q.userID == message.from_user.id)
     if bool(dbquery.count()):
         bot.send_message(message.chat.id,
                          botconfig.USER_ALREDY_REGISTERED.format(
@@ -114,12 +116,172 @@ def handler_command_deluser(message: Message):
     return
 
 
+# Reply markup Keyboard
+@bot.message_handler(commands=['menu'])
+def markupConfig(message: Message):
+    # Main menu
+    main_menu = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    mainbtn1 = KeyboardButton('Погода')
+    mainbtn2 = KeyboardButton('Настройки')
+    main_menu.add(mainbtn1, mainbtn2)
+    # Configuration menu
+    config_menu = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    configbtn1 = KeyboardButton('Регистрация')
+    configbtn2 = KeyboardButton('Удаление')
+    configbtn3 = KeyboardButton('Показать настройки')
+    configbtn4 = KeyboardButton('Месторасположение')
+    configbtn5 = KeyboardButton('Тип прогноза')
+    configbtn6 = KeyboardButton('Уведомление')
+    configbtn7 = KeyboardButton('Назад в главное меню')
+    config_menu.add(configbtn1, configbtn2, configbtn3, configbtn4, configbtn5, configbtn6, configbtn7)
+    # City menu
+    сity_menu = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    сitybtn1 = KeyboardButton('Указать место')
+    сitybtn2 = KeyboardButton('Определить место')
+    сitybtn3 = KeyboardButton('Указать долготу')
+    сitybtn4 = KeyboardButton('Указать широту')
+    сitybtn5 = KeyboardButton('Назад в главное меню')
+    сity_menu.add(сitybtn1, сitybtn2, сitybtn3, сitybtn4, сitybtn5)
+    # Type of weather
+    weather_menu = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    weatherbtn1 = KeyboardButton('Forecast')
+    weatherbtn2 = KeyboardButton('Hourly')
+    weatherbtn3 = KeyboardButton('Dayli')
+    weatherbtn4 = KeyboardButton('Назад в главное меню')
+    weather_menu.add(weatherbtn1, weatherbtn2, weatherbtn3, weatherbtn4)
+    # Notice
+    notice_menu = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    noticebtn1 = KeyboardButton('Каждый час')
+    noticebtn2 = KeyboardButton('Утром и вечером')
+    noticebtn3 = KeyboardButton('Отключить')
+    noticebtn4 = KeyboardButton('Назад в главное меню')
+    notice_menu.add(noticebtn1, noticebtn2, noticebtn3, noticebtn4)
+
+    def noticeMenu(message: Message):
+        text = message.text
+        if text == 'Каждый час':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Утром и вечером':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Отключить':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Назад в главное меню':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        else:
+            return
+
+    def weatherMenu(message: Message):
+        text = message.text
+        if text == 'Forecast':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Hourly':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Dayli':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Назад в главное меню':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        else:
+            return
+
+    def сityMenu(messaga: Message):
+        text = message.text
+        if text == 'Указать место':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Определить место':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Указать долготу':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Указать широту':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Назад в главное меню':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        else:
+            return
+
+    def configMenu(message: Message):
+        text = message.text
+        if text == 'Регистрация':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Удаление':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Показать настройки':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Месторасположение':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА', reply_markup=сity_menu)
+            bot.register_next_step_handler(msg, сityMenu)
+        elif text == 'Тип прогноза':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Уведомление':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Назад в главное меню':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        else:
+            return
+
+    def mainMenu(message: Message):
+        text = message.text
+        if text == 'Погода':
+            msg = bot.reply_to(message, 'ЗАГЛУШКА')
+            bot.register_next_step_handler(msg, markupConfig)
+        elif text == 'Настройки':
+            msg = bot.reply_to(message, 'Настройки', reply_markup=config_menu)
+            bot.register_next_step_handler(msg, configMenu)
+        else:
+            return
+
+    msg = bot.reply_to(message, "Выберете пункт меню", reply_markup=main_menu)
+    bot.register_next_step_handler(msg, mainMenu)
+
+
+bot.enable_save_next_step_handlers(delay=2)
+bot.load_next_step_handlers()
+
+
+@bot.message_handler(commands=['info'])
+@bot.edited_message_handler(commands=['info'])
+def handler_command_info(message: Message):
+    dbquery = UsersSettings.select(
+        UsersSettings.q.user == message.from_user.id)
+    if bool(dbquery.count()):
+        bot.send_message(message.chat.id, botconfig.USER_INFO.format(
+            dbquery[0].user,
+            dbquery[0].userTown,
+            dbquery[0].townLon,
+            dbquery[0].townLat,
+            dbquery[0].remindTime,
+            dbquery[0].remindDay,
+            dbquery[0].remindQuantity))
+    else:
+        bot.send_message(message.chat.id, botconfig.USER_NOT_EXIST.format(
+            message.from_user.username,
+            message.from_user.id))
+    return
+
+
 @bot.message_handler(commands=['chepetsk'])
 @bot.edited_message_handler(commands=['chepetsk'])
 def handler_command_chepetsk(message: Message):
     result = search_weather(town='Kirovo-Chepetsk', option='current')
     if result['cod'] == 200:
-        print(result['sys']['country'])
         write_current(result)
         dbquery = CurrentWeather.select(
             CurrentWeather.q.dateTime == result['dt'])
@@ -291,24 +453,6 @@ def handler_command_text(message: Message):
         bot.send_message(message.chat.id, botconfig.INFO_NOT_FOUND)
     return
 
-
-# Inline mode search weather in Kirov or Kirovo-Chepetsk
-@bot.inline_handler(lambda query: query.query in ['Ки', 'Ki', 'ки', 'ki'])
-def query_text(inline_query):
-    try:
-        kirov = InlineQueryResultArticle(
-            '1', 'Киров', InputTextMessageContent('Киров'))
-        kirovochepetsk = InlineQueryResultArticle(
-            '2', 'Кирово-Чепецк', InputTextMessageContent('Кирово-Чепецк'))
-        bot.answer_inline_query(inline_query.id, [kirov, kirovochepetsk])
-    except Exception as e:
-        print(e)
-
-
-# Echo replay
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
 
 print('StepTelegramBot is running')
 # RUN
