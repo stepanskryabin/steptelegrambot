645a5c93fa62e86c7f5b5f4b154440b410e74137
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-06-07 16:07:49 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,11 +4,11 @@ import sqlobject as orm
 import time
 import click
 
-import botconfig
-import models
-from botbase import update_city_list
+from app.config import LOCAL_SQLITE
+import app.models as models
 
-connection = orm.connectionForURI(botconfig.LOCAL_SQLITE)
+
+connection = orm.connectionForURI(LOCAL_SQLITE)
 orm.sqlhub.processConnection = connection
 
 
@@ -18,35 +18,17 @@ orm.sqlhub.processConnection = connection
 def main(table):
     if table == "create":
         start_time = time.process_time()
-        models.Users.createTable(ifNotExists=True)
-        models.UsersSettings.createTable(ifNotExists=True)
-        models.CurrentWeather.createTable(ifNotExists=True)
-        models.ForecastWeather.createTable(ifNotExists=True)
-        models.OnecallWeather.createTable(ifNotExists=True)
-        models.CityList.createTable(ifNotExists=True)
+        models.User.createTable(ifNotExists=True)
         print(f"Table is createt at: {time.process_time() - start_time} sec.")
+        return
     elif table == "delete":
         start_time = time.process_time()
-        models.Users.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.UsersSettings.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.CurrentWeather.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.ForecastWeather.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.OnecallWeather.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.CityList.dropTable(
+        models.User.dropTable(
             ifExists=True, dropJoinTables=True, cascade=True)
         print(f"Table is deleted at: {time.process_time() - start_time} sec.")
-    elif table == 'update_city':
-        start_time = time.process_time()
-        update_city_list(botconfig.CITY_LIST)
-        print(f'List of cities is loaded at: \
-            {time.process_time() - start_time} sec.')
-    else:
         return
+    else:
+        return print("ERROR. Function \'--table\' did not work")
 
 
 if __name__ == "__main__":
