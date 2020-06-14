5858167fc8b52ebc217697953b2ba0ac3795136b
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-06-14 17:10:27 +0300

    Update parser_tk.py

--- plug/parser_tk.py
+++ plug/parser_tk.py
@@ -2,23 +2,30 @@
 # Autor: Stepan Skriabin - stepan.skrjabin@gmail.com
 # Search goods in textile-club.ru
 
-import requests
 from bs4 import BeautifulSoup as bs
 from requests_html import HTMLSession
 
 
+HEADERS = {
+    'accept': '*/*',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
+          }
+
 SITE_URL = 'https://textile-club.ru/search/?'
-session = HTMLSession()
 
-#request1 = requests.get(SITE_URL, params={'q': 'Штаны 0207'})
-#request2 = requests.get(SITE_URL, params={'q': 'Штаны 0208'})
-request3 = session.get(SITE_URL, params={'q': 'Штаны 0207'})
-request4 = session.get(SITE_URL, params={'q': 'Штаны 0208'})
-#soup1 = bs(request1.text)
-#soup2 = bs(request2.text)
-soup3 = request3.html.find('div')
-soup4 = request4.html.find('a')
 
-print(soup3)
-print()
-print(soup4)
+def tk_parser(url: str, search: str, headers: dict) -> None:
+    session = HTMLSession()
+    request = session.get('https://textile-club.ru/search/?q=Штаны+0207')
+    print(request.status_code)
+    if request.status_code == 200:
+        request.html.render(timeout=20.0)
+        a = request.html.find('.element-name')
+        b = a[0].find('a')
+        print(b[0].links)
+        print(b[0].text)
+    else:
+        pass
+
+
+tk_parser(SITE_URL, 'Штаны 0207', HEADERS)
